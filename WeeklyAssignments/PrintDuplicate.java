3f7ee883f4ac46948f654546efd6bb2b48b0dcaa
DeoWilson <72688342+DeoWilson@users.noreply.github.com>
2020-10-13 08:33:42 +0530

    Update PrintDuplicate.java

--- WeeklyAssignments/PrintDuplicate.java
+++ WeeklyAssignments/PrintDuplicate.java
@@ -1,3 +1,4 @@
+
 package WeeklyAssignments;
 
 public class PrintDuplicate {
@@ -5,23 +6,26 @@ public class PrintDuplicate {
 	public static void main(String[] args) {
 		int[] a = {14,12,13,11,15,14,18,16,17,19,18,17,20};
 		int[] b = a;
-		int t=0;
+		int t=0;		
 		int count= a.length;
 		for(int i=0; i < count; i++)
 		{
+			
 			for (int j = 0; j < count; j++) {
 				
 				if(a[i] == b[j])
 				{
 					t=t+1;
 				}
-						
-			}
-			if (t>0) {
+				
+				if (t>1) {
 				t=0;
 				System.out.println(a[i]);
-				
+					
+				}
+						
 			}
+			
 	}
 	}
 
